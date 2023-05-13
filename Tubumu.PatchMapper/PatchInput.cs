a2b70a2c236944589ce94ea340b97793502e25a9
alby <alby@live.cn>
2023-05-13 17:46:39 +0800

    Cleanup

--- Tubumu.PatchMapper/PatchInput.cs
+++ Tubumu.PatchMapper/PatchInput.cs
@@ -3,10 +3,8 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Tubumu.PatchMapper
 {
-    public abstract class PatchInput : INotifyPropertyChanged
+    public abstract class PatchInput
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
-
         [BindNever]
         public ICollection<string>? PatchKeys { get; set; }
     }
